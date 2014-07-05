b257c01af9cd590aef81a01cff53831359532f39
Jordan Hudgens <jordan@wow.com>
2014-07-05 11:39:50 -0500

    Initial Commit

---
+++ BlocBrowser/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  BlocBrowser
+//
+//  Created by Jordan Hudgens on 7/5/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
