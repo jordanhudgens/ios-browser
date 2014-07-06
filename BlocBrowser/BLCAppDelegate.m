a45e06e46ee48c40d3c1c86d8e515eef2fbe8561
Jordan Hudgens <jordan@wow.com>
2014-07-05 19:36:37 -0500

    Automatically clear history when the app is closed

--- BlocBrowser/BLCAppDelegate.m
+++ BlocBrowser/BLCAppDelegate.m
@@ -13,6 +13,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Welcome!", @"Welcome title")
+                                                    message:NSLocalizedString(@"Get excited to use the best web browser ever!", @"Welcome comment")
+                                                   delegate:nil
+                                          cancelButtonTitle:NSLocalizedString(@"OK, I'm excited!", @"Welcome button title") otherButtonTitles:nil];
+    [alert show];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
@@ -28,6 +35,9 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    UINavigationController *navigationVC = (UINavigationController *)self.window.rootViewController;
+    BLCWebBrowserViewController *browserVC = [[navigationVC viewControllers] firstObject];
+    [browserVC resetWebView];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
